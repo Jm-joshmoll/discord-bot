b93783d6d344fab790aade898c0e59da97575ec5
Josh Waters <Jm.joshmoll@gmail.com>
2023-10-04 09:38:43 +0100

    Update ping.js
    Improved comments

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -6,7 +6,11 @@ module.exports = {
 
     // Callback function to execute when the command is invoked
     callback: async (client, interaction) => {
-        await interaction.deferReply();
+        
+        // Placeholder reply (<application> is thinking... message) whilst the bot is executing the task
+        await interaction.deferReply(); // deferReply({ ephemeral: true }) if you want the reply to be hidden to everyone else
+        
+        // Fetches interaction reply and compares the created timestamp of the reply to the initial interaction
         const reply = await interaction.fetchReply();
         const ping = reply.createdTimestamp - interaction.createdTimestamp;
 
